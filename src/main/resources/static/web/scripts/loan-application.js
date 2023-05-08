d3ddbe83a3f5f493c4e1c530af7d4842015dc82c
Agustin Martinez <gutin97agustin.martinez@gmail.com>
2023-05-08 18:58:29 -0300

    Finish: loan application page

--- src/main/resources/static/web/scripts/loan-application.js
+++ src/main/resources/static/web/scripts/loan-application.js
@@ -23,12 +23,15 @@ createApp({
         axios.get('http://localhost:8080/api/loans')
         .then(response =>{
             this.data = response.data
+            // console.log(this.calcularInteres(this.data, 60 ,"MORTGAGE" ))
+            
+            // console.log(response.data)
         })
         .catch(err => console.log(err))
         axios.get('http://localhost:8080/api/clients/current')
         .then(response =>{
             this.client = response.data.accounts
-            console.log(response.data.accounts)
+            // console.log(response.data.accounts)
         })
     },
     methods: {
@@ -61,25 +64,25 @@ createApp({
             for (let i = 0; i <this.data.length; i++) {
                 if (this.data[i].name === this.type) {
                     this.payments = this.data[i]
-                    // this.chosenLoan = this.data
-                    console.log(this.data[i])
                 }
             }
         },
         transferir(){
             Swal.fire({
-                title: 'Sure you want to transfer?',
-                text: "You won't be able to revert this",
+                title: 'Sure you want to acquire this loan?',
+                text: `${parseInt((this.calcularInteres(this.data, this.chosenPayment , this.type) * this.monto).toFixed(0))}`,
+                text:`You'll pay ${this.chosenPayment} payments of $ ${(((this.calcularInteres(this.data, this.chosenPayment , this.type) * this.monto))/this.chosenPayment).toFixed(2)} each. For a total of $ ${parseInt((this.calcularInteres(this.data, this.chosenPayment , this.type) * this.monto).toFixed(0))}`,
                 icon: 'warning',
                 showCancelButton: true,
                 confirmButtonColor: '#3085d6',
                 cancelButtonColor: '#d33',
-                confirmButtonText: 'Yes, transfer it'
-            }).then((result) => {
+                confirmButtonText: 'Yes'
+            })
+            .then((result) => {
                 if (result.isConfirmed) {
                     Swal.fire(
-                        'Transferred',
-                        'Succesful transaction',
+                        'Success',
+                        'You have a new loan',
                         'success'
                     )
                     axios.post('/api/loans',{ id: this.payments.id, amount: this.monto, payments: this.chosenPayment, destinationAccount: this.cuentaDestino})
@@ -99,12 +102,26 @@ createApp({
                 }
             })
         },
-        // available(){
-        //     for (let i = 0; i < this.data.accounts.length; i++) {
-        //         if(this.data.accounts[i].number == this.cuentaOrigen){
-        //             this.availableAmount = this.data.accounts[i].balance
-        //         }
-        //     }
-        // }
+        calcularInteres(lista, valor, type) {
+            let sum = 1
+            if(type == "MORTGAGE"){
+                sum += 0.2
+            } else if (type == "PERSONAL"){
+                sum += 0.1
+            } else if(type == "AUTOMOTIVE"){
+                sum += 0.15
+            }
+            for (let i = 0; i < lista.length; i++) {
+                if (lista[i].name === type) {
+                    for (let j = 0; j < lista[i].payments.length; j++) {
+                        if(lista[i].payments[j] == valor){
+                            sum += (j + 1) * 0.05
+                            return sum
+                        }
+                    }
+                }
+            }
+            return 0.0;
+        }
     }
 }).mount("#app")
